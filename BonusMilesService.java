0ce30054f33d8ff9bc91709f852039975acfcadb
Tyurina Olga <tyurinaolga19@gmail.com>
2021-05-20 19:14:39 +0300

    added files

---
+++ BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+
+    public int calculateBonus(int cost) {
+        int miles = 20;
+        int bonus;
+        bonus = cost / miles;
+        return bonus;
+    } }
